0ee154470477a16fcef431251f0247042363c062
Manuel Reimer <manuel.reimer@gmx.de>
2024-04-08 19:35:02 +0200

    Remove all HTML tags within headers
    HTML tags make the GitHub anchor generation unpredictable

--- process_manpage_html.py
+++ process_manpage_html.py
@@ -35,13 +35,15 @@ body = re.sub(r'<a [^>]+>(.+?)</a>', r'\1', body, flags=re.DOTALL)
 body = re.sub(r'<([a-z0-9]+)(?: (?:id|class)="[^"]+")+>', r'<\1>', body)
 
 # Wrap all <dt> contents into additional <h3> tags
-body = re.sub(r'<dt>(?:<b>)?(.*?)(?:</b>)?</dt>', r'<dt><h3>\1</h3></dt>', body, flags=re.DOTALL)
+body = re.sub(r'<dt>(.*?)</dt>', r'<dt><h3>\1</h3></dt>', body, flags=re.DOTALL)
 
+# Remove all HTML tags within headers
 # Replace series of space characters in headers with a single space character
 # Note: Two RegEx stacked with the first using a "positive lookbehind"
-body = re.sub(r'(?<=<h[1-3]>)([^<]+)',
-              lambda match: re.sub(r'\s+', r' ', match.group(1)),
-              body)
+body = re.sub(r'(?<=<h[1-3]>)(.+?)(?=</h[1-3]>)',
+              lambda match: re.sub(r'\s+', r' ',
+                                   re.sub(r'<[^>]+>', r'', match.group(1))),
+              body, flags=re.DOTALL)
 
 # Drop "<br/>" within "<pre>...</pre>" (bug in mandoc?)
 body = re.sub(r'(?<=<pre>)(.+?)(?=</pre>)',
